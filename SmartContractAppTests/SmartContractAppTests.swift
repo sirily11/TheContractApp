134bcd372c62f48960040958e7ff8cd73816a45a
sirily11 <32106111+sirily11@users.noreply.github.com>
2025-11-08 23:43:27 +0800

    feat: add script to run tests (#1)

--- SmartContractAppTests/SmartContractAppTests.swift
+++ SmartContractAppTests/SmartContractAppTests.swift
@@ -5,13 +5,11 @@
 //  Created by Qiwei Li on 11/5/25.
 //
 
-import Testing
 @testable import SmartContractApp
+import Testing
 
 struct SmartContractAppTests {
-
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
--- SmartContractAppUITests/SmartContractAppUITests.swift
+++ SmartContractAppUITests/SmartContractAppUITests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class SmartContractAppUITests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -29,13 +28,5 @@ final class SmartContractAppUITests: XCTestCase {
         app.launch()
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
     }
 }
